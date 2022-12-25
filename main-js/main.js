4da0317f81d7efa7997352eca722ea8f00a25b56
LegionDev370 <llegion630@gmail.com>
2022-12-25 17:43:36 +0500

    add sections

--- main-js/main.js
+++ main-js/main.js
@@ -1,5 +1,9 @@
 const hamburgerEl = document.querySelector(".hamburger-menu");
 const mobileMenuEl = document.querySelector(".mobile-menu");
+const mobileMenuClose = document.querySelector(".mobile-menu__close");
 hamburgerEl.onclick = function (event) {
-  mobileMenuEl.style.display = "block"
+  mobileMenuEl.style.display = "block";
 };
+mobileMenuClose.onclick = function(){
+  mobileMenuEl.style.display = "none"
+}
