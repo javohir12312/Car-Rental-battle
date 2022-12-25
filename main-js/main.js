62f375f17a7d869c6aa9837b8a93491205c5261b
LegionDev370 <llegion630@gmail.com>
2022-12-25 19:13:53 +0500

    added dark mode

--- main-js/main.js
+++ main-js/main.js
@@ -1,9 +1,33 @@
 const hamburgerEl = document.querySelector(".hamburger-menu");
 const mobileMenuEl = document.querySelector(".mobile-menu");
 const mobileMenuClose = document.querySelector(".mobile-menu__close");
+const body = document.querySelector("body")
+const loginBtn = document.querySelector(".navbar__login-btn")
+const modalLogin = document.querySelector(".modal__login")
+const modalLoginClose = document.querySelector(".modal__login-close")
+const mobileMenuBtnLogin = document.querySelector(".mobile-menu__footer-login-btn")
+const mode = document.querySelector(".navbar__dark-mode")
+
+mode.onclick = function(){
+  body.classList.toggle("dark")
+}
+
+
 hamburgerEl.onclick = function (event) {
   mobileMenuEl.style.display = "block";
+  body.style.overflow = "hidden"
 };
 mobileMenuClose.onclick = function(){
+  mobileMenuEl.style.display = "none"
+  body.style.overflo = "auto"
+}
+loginBtn.onclick = function(){
+  modalLogin.style.top = "50%"
+}
+modalLoginClose.onclick = function(){
+  modalLogin.style.top = "-50%"
+}
+mobileMenuBtnLogin.onclick = function(){
+  modalLogin.style.top = "50%"
   mobileMenuEl.style.display = "none"
 }
